0596c427ff54b5a966b0afbc0c48d32ea9fff9c6
Tihetna1891 <102287467+Tihetna1891@users.noreply.github.com>
2022-06-16 11:29:48 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - preints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+© 2022 GitHub, Inc.
 
